3abcf6d80a443facc3aed79ad333334673d6c2b5
Thomas Queste <tom@tomsquest.com>
2022-11-01 06:54:29 +0100

    refactor(test): simplify some assertions
    Signed-off-by: Thomas Queste <tom@tomsquest.com>

--- src/mediathequeroubaix/get_loans/base64decode_test.py
+++ src/mediathequeroubaix/get_loans/base64decode_test.py
@@ -7,9 +7,9 @@ def test_ok() -> None:
     assert base64decode("YWJj") == Success("abc")
 
 
-def test_invalid() -> None:
-    assert str(base64decode("[]{}").failure()) == "Non-base64 digit found"
-
-
 def test_empty() -> None:
     assert base64decode("") == Success("")
+
+
+def test_invalid() -> None:
+    assert str(base64decode("[]{}")) == "<Failure: Non-base64 digit found>"
