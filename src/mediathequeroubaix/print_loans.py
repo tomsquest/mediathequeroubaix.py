fff49b57c4be9d8181d76ece10f03a969360b032
Thomas Queste <tom@tomsquest.com>
2022-11-06 08:38:50 +0100

    feat: display nice looking table of loans (using Rich's Table)
    Signed-off-by: Thomas Queste <tom@tomsquest.com>

--- src/mediathequeroubaix/print_loans.py
+++ src/mediathequeroubaix/print_loans.py
@@ -1,6 +1,8 @@
 from requests import Session
 from returns.io import IOFailure, IOSuccess
 from returns.result import Success
+from rich import box, print
+from rich.table import Table
 
 from mediathequeroubaix.get_loans.get_loans import get_loans
 from mediathequeroubaix.get_loans.loan import Loan
@@ -15,10 +17,24 @@ def print_loans(*, session: Session) -> None:
 
 
 def _print(loans: list[Loan]) -> None:
-    total = len(loans)
-    print(f"Number of loans: {total}")
+    table = Table(
+        title=f"{len(loans)} LOANS",
+        title_style="bold magenta",
+        box=box.HEAVY_EDGE,
+        expand=True,
+    )
+    table.add_column("#", justify="center")
+    table.add_column("Title", style="blue", ratio=2)
+    table.add_column("Due date", justify="center")
+    table.add_column("Renewable")
+
     for index, loan in enumerate(loans):
-        renewable = "renewable" if loan.renewable else "NOT renewable"
-        print(
-            f"- [{index+1:>2}/{total}] {loan.title}, due on: {loan.date_due}, {renewable}"
+        renewable = "✅" if loan.renewable else "❌"
+        table.add_row(
+            f"{index+1:>2}/{len(loans)}",
+            loan.title,
+            f"{loan.date_due:%d/%m/%Y}",
+            renewable,
         )
+
+    print(table)
