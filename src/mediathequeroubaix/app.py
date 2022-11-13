e48d4ca471462237a411537eb6a82f590950f04b
Thomas Queste <tom@tomsquest.com>
2022-11-13 06:36:02 +0100

    refactor: extract command for "loans list"
    Signed-off-by: Thomas Queste <tom@tomsquest.com>

--- src/mediathequeroubaix/app.py
+++ src/mediathequeroubaix/app.py
@@ -1,30 +1,10 @@
 import typer
-from returns.io import IOFailure, IOSuccess
-from returns.result import Success
 
-from mediathequeroubaix import config
-from mediathequeroubaix.authenticate import authenticate
-from mediathequeroubaix.config import get_config
-from mediathequeroubaix.print_loans import print_loans
+from mediathequeroubaix import config, loans
 
-app = typer.Typer(no_args_is_help=True, add_completion=False)
+app = typer.Typer(add_completion=False, no_args_is_help=True)
 app.add_typer(config.app, name="config", no_args_is_help=True)
-
-
-@app.command()
-def loans() -> None:
-    config = get_config()
-    if not config.users:
-        print("No users in config file")
-        raise typer.Exit(code=1)
-
-    first_user = config.users[0]
-    print(f"Getting loans of user: {first_user.login}")
-    match authenticate(first_user.login, first_user.password):
-        case IOSuccess(Success(authenticated_session)):
-            print_loans(authenticated_session)
-        case IOFailure(failure):
-            print("❌ FAILURE!", failure)
+app.add_typer(loans.app, name="loans", no_args_is_help=True)
 
 
 if __name__ == "__main__":
--- src/mediathequeroubaix/loans.py
+++ src/mediathequeroubaix/loans.py
@@ -1,11 +1,33 @@
+import typer
 from returns.io import IOFailure, IOSuccess
 from returns.result import Success
 from rich import box, print
 from rich.table import Table
 
+from mediathequeroubaix.authenticate import authenticate
+from mediathequeroubaix.config import get_config
 from mediathequeroubaix.get_loans.get_loans import get_loans
 from mediathequeroubaix.get_loans.loan import Loan
 from mediathequeroubaix.login.authenticated_session import AuthenticatedSession, User
+
+app = typer.Typer()
+
+
+@app.command(name="list")
+def list_loans() -> None:
+    config = get_config()
+
+    if not config.users:
+        print("No users in config file")
+        raise typer.Exit(code=1)
+    first_user = config.users[0]
+
+    print(f"Getting loans of user: {first_user.login}")
+    match authenticate(first_user.login, first_user.password):
+        case IOSuccess(Success(authenticated_session)):
+            print_loans(authenticated_session)
+        case IOFailure(failure):
+            print("❌ FAILURE!", failure)
 
 
 def print_loans(session: AuthenticatedSession) -> None:
