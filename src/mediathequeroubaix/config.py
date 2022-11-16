6d62f4fdb946e7142d71f90971b282455a8f1138
Thomas Queste <tom@tomsquest.com>
2022-11-16 06:56:47 +0100

    feat: pretty print config (on create, on show)
    Signed-off-by: Thomas Queste <tom@tomsquest.com>

--- src/mediathequeroubaix/config.py
+++ src/mediathequeroubaix/config.py
@@ -6,7 +6,9 @@ from returns.io import IOFailure, IOResultE, IOSuccess
 from returns.pipeline import flow
 from returns.pointfree import bind_ioresult
 from returns.result import Success
-from rich.pretty import pprint
+from rich import print
+from rich.panel import Panel
+from rich.pretty import Pretty
 
 APP_NAME = "mediathequeroubaix"
 app = typer.Typer()
@@ -16,9 +18,7 @@ app = typer.Typer()
 def show() -> None:
     match get_config():
         case IOSuccess(Success(config)):
-            print(f"Config located at: {_get_config_path()}")
-            print("")
-            pprint(config, expand_all=True)
+            _show(_get_path(), config)
         case IOFailure(failure):
             print(failure)
             raise typer.Exit(code=1)
@@ -26,13 +26,17 @@ def show() -> None:
 
 @app.command()
 def create() -> None:
-    config_path = _get_config_path()
+    config_path = _get_path()
     if config_path.is_file():
-        print(f"Config file already exists: {config_path}")
+        print()
+        print(
+            f"[bold][yellow] Config file already exists:[/yellow] {_get_path()}[/bold]"
+        )
+        print()
         raise typer.Exit(code=1)
     else:
-        _create_sample_config(config_path)
-        _show_config(config_path)
+        config = _create_sample_config(config_path)
+        _show(config_path, config)
 
 
 class User(BaseModel):
@@ -49,38 +53,41 @@ class Config(BaseSettings):
 
 def get_config() -> IOResultE[Config]:
     return flow(
-        _get_config_path(),
-        _is_path_readable,
-        bind_ioresult(_read_as_config),
+        _get_path(),
+        _is_readable,
+        bind_ioresult(_read),
     )
 
 
-def _is_path_readable(path: Path) -> IOResultE[Path]:
+def _is_readable(path: Path) -> IOResultE[Path]:
     if path.is_file():
         return IOSuccess(path)
     return IOFailure(ValueError(f"File {path} is missing or not readable"))
 
 
-def _get_config_path() -> Path:
+def _get_path() -> Path:
     app_path = Path(typer.get_app_dir(APP_NAME))
     return app_path / "config.json"
 
 
-def _read_as_config(path: Path) -> IOResultE[Config]:
+def _read(path: Path) -> IOResultE[Config]:
     try:
         return IOSuccess(Config.parse_file(path))
     except Exception:
         return IOFailure(ValueError(f"Invalid configuration: {path}"))
 
 
-def _show_config(config_path: Path) -> None:
-    print(f"Config located at: {config_path}")
-    config = _read_as_config(config_path)
-    print("")
-    pprint(config, expand_all=True)
+def _show(config_path: Path, config: Config) -> None:
+    print()
+    print(f"[bold][yellow] Config:[/yellow] {config_path}[/bold]")
+    print()
+    panel = Panel(Pretty(config, expand_all=True))
+    print(panel)
+    print()
 
 
-def _create_sample_config(config_path: Path) -> None:
+def _create_sample_config(config_path: Path) -> Config:
     config_path.parent.mkdir(parents=True, exist_ok=True)
     sample_config = Config(users=[User(login="X001002003", password="password00")])
     config_path.write_text(sample_config.json())
+    return sample_config
