8c617467cf95bae21f4af070c71b28efa32f2b94
Thomas Queste <tom@tomsquest.com>
2025-07-19 07:11:56 +0200

    chore: add a main declaration in main.py for easier running

--- src/mediathequeroubaix/main.py
+++ src/mediathequeroubaix/main.py
@@ -5,3 +5,6 @@ from mediathequeroubaix import config, loans
 app = typer.Typer(add_completion=False, no_args_is_help=True)
 app.add_typer(config.app, name="config", no_args_is_help=True)
 app.add_typer(loans.app, name="loans", no_args_is_help=True)
+
+if __name__ == '__main__':
+    app(prog_name=config.APP_NAME)
